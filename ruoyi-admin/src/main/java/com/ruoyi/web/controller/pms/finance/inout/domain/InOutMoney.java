658e60ffd64973897eb53870fec805538aab4464
jgc <jiangguangchao157@163.com>
2022-11-29 10:34:25 +0800

    收支明细新增字段

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/pms/finance/inout/domain/InOutMoney.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/pms/finance/inout/domain/InOutMoney.java
@@ -10,9 +10,9 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 收支明细对象 inoutmoney
- * 
+ *
  * @author ruoyi
- * @date 2022-11-27
+ * @date 2022-11-29
  */
 public class InOutMoney extends BaseEntity
 {
@@ -38,60 +38,159 @@ public class InOutMoney extends BaseEntity
     @Excel(name = "收款/付款时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
     private Date payTime;
 
-    public void setId(Long id) 
+    /** 用途大类 */
+    @Excel(name = "用途大类")
+    private String useMasterType;
+
+    /** 支付/收款方式 */
+    @Excel(name = "支付/收款方式")
+    private String payType;
+
+    /** 付款人 */
+    @Excel(name = "付款人")
+    private String payer;
+
+    /** 收款人 */
+    @Excel(name = "收款人")
+    private String payee;
+
+    /** 收款账号 */
+    @Excel(name = "收款账号")
+    private String colAccount;
+
+    /** 付款账号 */
+    @Excel(name = "付款账号")
+    private String payAccount;
+
+    /** 票据图片 */
+    @Excel(name = "票据图片")
+    private String billPic;
+
+    public void setId(Long id)
     {
         this.id = id;
     }
 
-    public Long getId() 
+    public Long getId()
     {
         return id;
     }
-    public void setInOutFlag(Long inOutFlag) 
+    public void setInOutFlag(Long inOutFlag)
     {
         this.inOutFlag = inOutFlag;
     }
 
-    public Long getInOutFlag() 
+    public Long getInOutFlag()
     {
         return inOutFlag;
     }
-    public void setType(String type) 
+    public void setType(String type)
     {
         this.type = type;
     }
 
-    public String getType() 
+    public String getType()
     {
         return type;
     }
-    public void setAmount(BigDecimal amount) 
+    public void setAmount(BigDecimal amount)
     {
         this.amount = amount;
     }
 
-    public BigDecimal getAmount() 
+    public BigDecimal getAmount()
     {
         return amount;
     }
-    public void setPayTime(Date payTime) 
+    public void setPayTime(Date payTime)
     {
         this.payTime = payTime;
     }
 
-    public Date getPayTime() 
+    public Date getPayTime()
     {
         return payTime;
+    }
+    public void setUseMasterType(String useMasterType)
+    {
+        this.useMasterType = useMasterType;
+    }
+
+    public String getUseMasterType()
+    {
+        return useMasterType;
+    }
+    public void setPayType(String payType)
+    {
+        this.payType = payType;
+    }
+
+    public String getPayType()
+    {
+        return payType;
+    }
+    public void setPayer(String payer)
+    {
+        this.payer = payer;
+    }
+
+    public String getPayer()
+    {
+        return payer;
+    }
+    public void setPayee(String payee)
+    {
+        this.payee = payee;
+    }
+
+    public String getPayee()
+    {
+        return payee;
+    }
+    public void setColAccount(String colAccount)
+    {
+        this.colAccount = colAccount;
+    }
+
+    public String getColAccount()
+    {
+        return colAccount;
+    }
+    public void setPayAccount(String payAccount)
+    {
+        this.payAccount = payAccount;
+    }
+
+    public String getPayAccount()
+    {
+        return payAccount;
+    }
+    public void setBillPic(String billPic)
+    {
+        this.billPic = billPic;
+    }
+
+    public String getBillPic()
+    {
+        return billPic;
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("inOutFlag", getInOutFlag())
-            .append("type", getType())
-            .append("amount", getAmount())
-            .append("payTime", getPayTime())
-            .toString();
+                .append("id", getId())
+                .append("inOutFlag", getInOutFlag())
+                .append("type", getType())
+                .append("amount", getAmount())
+                .append("payTime", getPayTime())
+                .append("useMasterType", getUseMasterType())
+                .append("payType", getPayType())
+                .append("payer", getPayer())
+                .append("payee", getPayee())
+                .append("colAccount", getColAccount())
+                .append("payAccount", getPayAccount())
+                .append("billPic", getBillPic())
+                .append("remark", getRemark())
+                .toString();
     }
 }
